188c26c2145e34803e240a2be4d007113dd20c9a
1Peripheral <zblaxe@gmail.com>
2023-05-22 01:48:21 +0100

    Added some configuration so i can deploy it

--- e_commerce/wsgi.py
+++ e_commerce/wsgi.py
@@ -1,8 +1,4 @@
 """
-WSGI config for e_commerce project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
 For more information on this file, see
 https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 """
@@ -14,3 +10,5 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'e_commerce.settings')
 
 application = get_wsgi_application()
+
+app = application
